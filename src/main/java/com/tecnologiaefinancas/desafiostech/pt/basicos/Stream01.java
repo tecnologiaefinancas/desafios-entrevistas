a1958d1bfb0a1e63f487e1b7e04989b870942415
Natalia Feitosa <estudandocertificacoes@gmail.com>
2025-04-22 15:13:18 -0300

    Desafio Stream01

---
+++ src/main/java/com/tecnologiaefinancas/desafiostech/pt/basicos/Stream01.java
@@ -0,0 +1,29 @@
+package com.tecnologiaefinancas.desafiostech.pt.basicos;
+
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Stream01 {
+    /*
+     Desafio: Filtragem e Transformação com Streams
+     Você recebe uma lista de números inteiros. Sua tarefa é:
+    - Filtrar apenas os números pares.
+    - Elevar cada número ao quadrado.
+    - Ordenar os números em ordem crescente.
+    - Exibir os números formatados como uma única linha de saída.
+    */
+    public static void main (String[] args) {
+        List<Integer> numeros = Arrays.asList(3, 7, 8, 2, 10, 1, 5, 4);
+        List<Integer> resultado = numeros.stream()
+                .filter(n -> (n % 2 == 0))
+                .map(n -> n * n)
+                .sorted()
+                .toList();
+
+        System.out.println(resultado);
+    }
+
+
+}
