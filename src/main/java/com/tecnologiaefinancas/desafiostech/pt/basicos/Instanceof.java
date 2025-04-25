a8537e49717b3abcf35a5b112734dc9aef712dce
Natalia Feitosa <estudandocertificacoes@gmail.com>
2025-04-25 10:58:35 -0300

    Desafios Instanceof básico e intermediário.

---
+++ src/main/java/com/tecnologiaefinancas/desafiostech/pt/basicos/Instanceof.java
@@ -0,0 +1,29 @@
+package com.tecnologiaefinancas.desafiostech.pt.basicos;
+
+public class Instanceof {
+
+    /*
+    Desafio: Crie uma classe Instanceof que contém um métod0
+   identify(Object obj). Esse métod0 deve verificar se obj é uma instância
+   de String, Integer ou Double. Se for um desses tipos, exiba uma mensagem correspondente.
+     */
+
+    static void identify(Object obj) {
+        if(obj instanceof String) {
+            System.out.println("O objeto é uma String!");
+        } else if (obj instanceof Double){
+            System.out.println("O objeto é um Double!");
+        } else if (obj instanceof Integer){
+            System.out.println("O objeto é um Integer!");
+        } else {
+            System.out.println("Tipo desconhecido!");
+        }
+    }
+
+    public static void main (String[] args){
+        identify("Hello, world!");
+        identify(42);
+        identify(3.14);
+        identify(true); // Teste com um tipo não tratado.
+    }
+}
---
+++ src/main/java/com/tecnologiaefinancas/desafiostech/pt/intermediarios/InstanceofGame.java
@@ -0,0 +1,57 @@
+package com.tecnologiaefinancas.desafiostech.pt.intermediarios;
+
+public class InstanceofGame {
+
+    /*
+    Desafio: Crie uma classe InstanceofGame que simule um jogo de verificação
+    de tipos. Teremos três classes: Warrior, Mage e Rogue.
+    Um métod0 describeCharacter(Object character) deve identificar o tipo do personagem
+    e exibir uma mensagem diferente para cada um.
+     */
+
+
+    static void describeCharacter(Object character){
+        if (character instanceof Warrior){
+            System.out.println("Este é um Guerreiro!");
+            ((Warrior) character).fight();
+        } else if (character instanceof Mage){
+            System.out.println("Este é um Mago!");
+            ((Mage) character).castSpell();
+        } else if (character instanceof Rogue){
+            System.out.println("Esté é um Ladino!");
+            ((Rogue) character).sneak();
+        } else {
+            System.out.println("Personagem desconhecido!");
+        }
+    }
+
+    public static void main (String[] args){
+        Warrior warrior = new Warrior();
+        Mage mage = new Mage();
+        Rogue rogue = new Rogue();
+        String randomObject = "Isso não é um personagem válido!";
+
+        describeCharacter(warrior);
+        describeCharacter(mage);
+        describeCharacter(rogue);
+        describeCharacter(randomObject); // Teste com um tipo diferente;
+    }
+}
+
+class Warrior {
+    void fight() {
+        System.out.println("O guerreiro está lutando ferozmente");
+    }
+}
+
+class Mage {
+    void castSpell(){
+        System.out.println("O mago lança um feitiço poderoso!");
+    }
+}
+
+class Rogue {
+    void sneak(){
+        System.out.println("O ladino se mode nas sombras!");
+    }
+}
